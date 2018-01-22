9789329890b2b93352f8cd4d6c2a555c9f6709bf
Phanor Coll <phanorcoll@gmail.com>
2018-01-22 14:01:00 -0400

    initial structure

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './LoginForm.jsx';
+
+const App = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App; 
---
+++ app/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return (
+        <div>
+            <div>Login form</div>
+        </div>
+    );
+}
+
+export default LoginForm; 
